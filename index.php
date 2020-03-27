379161a2d1b3b7216b61bf10115a6d7730effad7
Alexis <alexdu9521@gmail.com>
2020-03-27 18:32:01 +0100

    Mise en place du slide

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require 'vendor/autoload.php';
+
+if (isset($_GET['p'])){
+    $page = $_GET['p'];
+}
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader, [
+    'cache' =>  false, // . '/tmp'
+]);
+
+if($page === 'home'){
+    echo $twig->render('home.twig');
+}
+
+if($page === 'admin'){
+    echo $twig->render('admin.html.twig');
+}
+
+if($page === 'register'){
+    echo $twig->render('register.html.twig');
+}
---
+++ src/login.js
@@ -0,0 +1,12 @@
+
+$('#login_link').click(function() {
+    $('#login').toggle();
+});
+
+
+$('#bloc_page_base').click(function () {
+
+    $('#login').css('display', 'none');
+
+});
+
